39cc36949c9cc442f06844ac8674f54dbec97af6
gustavokida <gustavokida@live.com>
2019-07-31 19:52:56 -0400

    Modifica o nome dos pacotes

--- cao-chupando-manga-domain/src/cao/chupando/manga/domain/Main.java
+++ cao-chupando-manga-domain/src/cao/chupando/manga/domain/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Cao.chupando.manga.domain;
+package cao.chupando.manga.domain;
 
 /**
  *
