80ca902897d096ba8c7d96827d9b418cfccc7a7a
gustavokida <gustavokida@live.com>
2019-08-07 21:00:03 -0400

    criado web api

---
+++ cao-chupando-manga-webapi/src/java/controller/CategoriaController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author gustavo
+ */
+@Path("/categoria")
+public class CategoriaController {
+ 
+    @GET
+    @Path("/")
+    public String index(){
+        return "teste";
+    }
+    
+}
